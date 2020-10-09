7f032acbcc21577155ccbff07389d9bae380fe8a
Jaeyeol Lee <rijgndqw012@gmail.com>
2020-10-10 00:38:52 +0900

    fix: firebase storage에 등록된 이미지들만 긁어오도록 수정

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_trial_app/create_page.dart';
@@ -30,19 +31,34 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   Widget _buildBody() {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // 3칸 짜리 열
-        childAspectRatio: 1.0, // 가로세로 비율
-        mainAxisSpacing: 1.0,
-        crossAxisSpacing: 1.0, // 가로 세로 간격
-      ), itemBuilder: (context, index) {
-        return _buildListItem(context, index);
-      }
+    return StreamBuilder(
+      stream: Firestore.instance.collection('awesome-post').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        var items = snapshot.data.documents ?? [];
+
+        return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // 3칸 짜리 열
+              childAspectRatio: 1.0, // 가로세로 비율
+              mainAxisSpacing: 1.0,
+              crossAxisSpacing: 1.0, // 가로 세로 간격
+            ),
+            itemCount: items.length, // items 의 갯수만큼 렌더링
+            itemBuilder: (context, index) {
+              return _buildListItem(context, items[index]);
+            }
+        );
+      },
     );
   }
 
-  Widget _buildListItem(BuildContext context, int index) {
-    return Image.network('https://picsum.photos/id/237/200/200', fit: BoxFit.cover);
+  Widget _buildListItem(BuildContext context, document) {
+    return Image.network(
+      document['photoUrl'],
+      fit: BoxFit.cover);
   }
 }
