493ad7ec76362a60eee0d54b90d3b8ca8d4f970a
Jaeyeol Lee <rijgndqw012@gmail.com>
2020-10-09 22:32:38 +0900

    Firebase 기반의 구글 인증 테스트 & 로그인 화면 구현

---
+++ lib/login_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatelessWidget {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Login Page",
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.all(50.0)
+            ),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {
+                _handleSignIn().then((user) {
+                  print(user);
+                });
+              },
+            )
+          ],
+        )
+      ),
+    );
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = (await _auth.signInWithCredential(
+      GoogleAuthProvider.getCredential(idToken: googleAuth.idToken, accessToken: googleAuth.accessToken)
+    )).user;
+
+    return user;
+  }
+}
